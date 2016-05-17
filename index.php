8f6adfd3220f1a759e5a3b3fc0a224e6dd603e4b
Eason <trueluo1987@gmail.com>
2016-05-17 17:24:41 +1000

    added edit form and update function

--- index.php
+++ index.php
@@ -3,39 +3,97 @@
 <head>
 <title>Angular</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<style type="text/css">
+body{
+	margin:0px;
+	padding:0px;
+}
+nav{
+}
+nav>ul{
+	padding:0px;
+	list-style-type:none;
+}
+nav>ul>li{
+	float:left;
+}
+nav>ul>li>a{
+	padding:10px 15px;
+	color:#000;
+	background-color:#FFF;
+	text-decoration: none;
+}
+nav>ul>li>a:hover{
+	color:#FFF;
+	background-color:#000;
+}
+</style>
 </head>
-<body ng-app="app" ng-init="original=items=[{'id':1,'name':'Eason','price':25.5,'stock':311},
-									{'id':2,'name':'Seyla','price':15,'stock':13},
-									{'id':3,'name':'Anna','price':500.00,'stock':43},
-									{'id':4,'name':'Tim','price':1.03,'stock':0},
-									{'id':5,'name':'Vey','price':88,'stock':6},
-									{'id':6,'name':'Travis','price':0.99,'stock':290},
-									{'id':7,'name':'Joachim','price':160.200001,'stock':25}]">
+<body ng-app="app">
 
-<div class="container" ng-controller="mycontroller">
-	<h2>Search Products</h2>
+<div ng-include="'include/header.html'"></div>
+									
+<div class="container-fluid" ng-controller="mycontroller">
 	<div class="row">
-		<div class="col-md-3">
-			<input type="search" class="form-control" placeholder="Search Products" ng-model="searchText" ng-change="search(searchText)">
+		<div class="col-md-8">
+			<h2>Search Products</h2>
+			<div class="row">
+				<div class="col-md-4">
+					<input type="search" class="form-control" placeholder="Search Products" ng-model="searchText" ng-change="search(searchText)">
+				</div>
+			</div>
+			<table class="table">
+				<thead>
+					<tr>
+						<th><a href="" ng-click="sortBy('id')">ID</a></th>
+						<th><a href="" ng-click="sortBy('name')">NAME</a></th>
+						<th><a href="" ng-click="sortBy('price')">PRICE</a></th>
+						<th><a href="" ng-click="sortBy('stock')">STOCK</a></th>
+					</tr>
+				</thead>
+				<tr ng-repeat="item in items | orderBy:predicate:reverse" ng-click="sendToForm(item)">
+					<td>{{item.id}}</td>
+					<td>{{item.name}}</td>
+					<td>{{item.price | currency}}</td>
+					<td>{{item.stock}}</td>
+				</tr>
+			</table>
+		</div>
+		<div class="col-md-4">
+			<h2>Edit Product</h2>
+			<form action="" class="form" ng-model="edit">
+				<div class="form-group">
+					<label>ID</label>
+					<input class="form-control" name="id" readonly="readonly" ng-model="edit.id">
+				</div>
+				<div class="form-group">
+					<label>NAME</label>
+					<input class="form-control" name="name" ng-model="edit.name">
+				</div>
+				<div class="form-group">
+					<label>PRICE</label>
+					<input class="form-control" name="price" ng-model="edit.price">
+				</div>
+				<div class="form-group">
+					<label>STOCK</label>
+					<input class="form-control" name="stock" ng-model="edit.stock">
+				</div>
+				<div class="form-group">
+					<a href="#add" class="btn btn-default"><span class="glyphicon glyphicon-plus">Add</span></a>
+					<a href="#update" class="btn btn-success"><span class="glyphicon glyphicon-floppy-disk">Save</span></a>
+				</div>
+			</form>
 		</div>
 	</div>
-	<table class="table">
-		<thead>
-			<tr>
-				<th><a href="" ng-click="sortBy('id')">ID</a></th>
-				<th><a href="" ng-click="sortBy('name')">NAME</a></th>
-				<th><a href="" ng-click="sortBy('price')">PRICE</a></th>
-				<th><a href="" ng-click="sortBy('stock')">STOCK</a></th>
-			</tr>
-		</thead>
-		<tr ng-repeat="item in items | orderBy:predicate:reverse">
-			<td>{{item.id}}</td>
-			<td>{{item.name}}</td>
-			<td>{{item.price | currency}}</td>
-			<td>{{item.stock}}</td>
-		</tr>
-	</table>
+	<div class="row">
+		<div class="col-md-12">
+			<h2></h2>
+			<p>{{message}}</p>
+		</div>	
+	</div>
 </div>
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 (function(){
-	angular.module('app',[]).controller('mycontroller', function($scope, $filter){
+	angular.module('app',[]).controller('mycontroller', function($scope, $http, $filter){
 		
+		$http.get('js/data.json').success(function(response){
+			$scope.original = $scope.items = response;
+		});
+		
+		$scope.edit = {};
 		$scope.sortState = {'id':'+','name':'+','price':'+','quality':'+'};
 		$scope.search = function(searchText){
 			$scope.items = $filter('filter')($scope.original, searchText);
@@ -10,6 +15,25 @@
 		    $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
 		    $scope.predicate = predicate;
 		}
+		
+		$scope.sendToForm = function (item){
+			for(prop in item){
+				$scope.edit[prop] = item[prop];
+			}
+		};
+		
+		$scope.update = function (){
+			for(var i=0;i<$scope.items.length;i++){
+				var item = $scope.items[i];
+				if(item.id==$scope.edit.id){
+					for(prop in $scope.edit){
+						item[prop] = $scope.edit[prop];
+					}
+					break;
+				}
+			} 
+		};
+		
 	});
 	
 	
